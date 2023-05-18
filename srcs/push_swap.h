7cdcc44b5c853d3f4e4e094637401d096e8af1c4
Theoli <theoli231@gmail.com>
2023-05-18 09:39:34 +0200

    removed unexistent old functions from header file

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tlivroze <tlivroze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 17:47:11 by tlivroze          #+#    #+#             */
-/*   Updated: 2023/05/18 09:29:13 by tlivroze         ###   ########.fr       */
+/*   Updated: 2023/05/18 09:37:47 by tlivroze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,14 +46,9 @@ typedef struct s_pile
 }				t_pile;
 
 int			verif(char **argv, int argc, t_data *data);
-int			get_position(t_pile *a, int value);
 int			get_size(t_pile **a);
-int			get_minimum(t_pile *a);
-int			ft_pilesize(t_pile *lst);
 int			find_biggest(t_pile *b);
-int			find_biggest_smaller(t_pile *b, int index);
 void		ft_pileadd_front(t_pile **lst, t_pile *new);
-void		ft_pileadd_back(t_pile **lst, t_pile *new);
 void		sa(t_pile *pile);
 void		sb(t_pile *pile);
 void		ss(t_pile *a, t_pile *b);
@@ -70,18 +65,13 @@ void		rotate_a(t_pile **a, t_moves best);
 void		rotate_b(t_pile **b, t_moves best);
 void		final_ra(t_pile **a);
 void		small_sort(t_pile **a);
-void		ft_piledelone(t_pile *lst);
 void		put_index(t_pile **a, t_data data);
 void		big_sort(t_pile **a, t_pile **b);
-void		do_final_rb(t_pile **b, int pos, int index);
 void		push_elem_b_to_a(t_pile **a, t_pile **b, t_moves best);
 bool		ft_issorted(t_pile	*a);
 t_pile		*ft_pilelast(t_pile *lst);
 t_pile		*ft_pilenew(int value, int index);
-t_moves		calc_moves_a(t_pile **a, t_moves curr);
-t_moves		calc_moves_b(t_pile **b, t_moves curr);
 t_moves		optimize_moves(t_moves curr);
-t_moves		calc_moves_b_bis(t_pile	*head, t_moves curr, int index, int size);
 t_moves		calc_best_moves_rev(t_pile **a, t_pile **b, t_moves best);
 t_moves		init(t_moves moves, int init, int index);
 t_moves		calc_a_rev_bis(t_pile	*head, t_moves curr, int index, int size);
